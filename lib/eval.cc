c215d1b12d5742f26612749a365c1d021684f3cf
Sirraide <aeternalmail@gmail.com>
2023-07-03 07:00:05 +0200

    [Refactor/Eval] CRTP black magic

--- lib/eval.cc
+++ lib/eval.cc
@@ -9,14 +9,17 @@ using namespace v8;
 
 namespace nhtml::detail {
 struct eval_impl {
+    using eval = eval_impl;
     std::unique_ptr<Platform> v8_platform;
     ArrayBuffer::Allocator* alloc;
     Isolate* isolate;
     parser& p;
     Persistent<ObjectTemplate> globl_tmpl;
     Persistent<ObjectTemplate> element_tmpl;
+    Persistent<ObjectTemplate> children_tmpl;
     Persistent<ObjectTemplate> attributes_tmpl;
     Persistent<ObjectTemplate> attributes_it_tmpl;
+    Persistent<ObjectTemplate> children_it_tmpl;
 
     /// Helper to export a global function. Must not be called outside the constructor.
     template <auto cb>
@@ -31,15 +34,79 @@ struct eval_impl {
         tm->Set(isolate, name, FunctionTemplate::New(isolate, wrapper));
     };
 
-    /// Accessors for elements.
-    class $element {
-        static auto ptr(const auto& info) -> element* {
-            return static_cast<element*>(
+    /// ===========================================================================
+    ///  Object template helpers.
+    /// ===========================================================================
+    using template_t = Persistent<ObjectTemplate> eval_impl::*;
+
+    /// CRTP helper for types that have a native handle pointer.
+    template <typename handle_ty, template_t obj_templ, std::size_t fields = 1>
+    requires (not std::is_pointer_v<handle_ty>)
+    struct object {
+        static auto handle(const auto& info) -> handle_ty* {
+            return static_cast<handle_ty*>(
                 Local<External>::Cast(info.Holder()->GetInternalField(0))->Value()
             );
         }
 
-    public:
+        using handle_type = handle_ty;
+        static constexpr template_t object_template = obj_templ;
+        static constexpr usz field_count = fields;
+    };
+
+    /// CRTP index-based iterator template. For internal use in register_template() only.
+    template <typename derived, typename handle_type>
+    struct iterator_base {
+        static void next(const FunctionCallbackInfo<Value>& info) {
+            auto I = info.GetIsolate();
+            HandleScope hs{I};
+
+            /// Get element.
+            auto e = static_cast<handle_type*>(
+                Local<External>::Cast(info.Holder()->GetInternalField(0))->Value()
+            );
+
+            /// Get index.
+            auto i = int(Local<Integer>::Cast(info.Holder()->GetInternalField(1))->Value());
+
+            /// Create return value.
+            auto obj = info.Holder();
+            auto ctx = I->GetCurrentContext();
+            auto ret = Object::New(I);
+            if (not derived::done(e, i)) {
+                obj->SetInternalField(1, Integer::New(I, i + 1));
+                ret->Set(ctx, S(I, "done"), False(I)).Check();
+                ret->Set(ctx, S(I, "value"), derived::value(I, e, i)).Check();
+            } else {
+                ret->Set(ctx, S(I, "done"), True(I)).Check();
+            }
+            info.GetReturnValue().Set(ret);
+        }
+    };
+
+    /// CRTP index-based iterator template for iterable types.
+    template <typename derived, template_t iterator_obj_templ>
+    struct iterable {
+        static void create_iterator(const FunctionCallbackInfo<Value>& info) {
+            auto I = info.GetIsolate();
+            HandleScope hs{I};
+
+            /// Create iterator.
+            auto e = derived::handle(info);
+            auto iter = instantiate(I, context(I)->*iterator_obj_templ);
+            iter->SetInternalField(0, External::New(I, e));
+            iter->SetInternalField(1, Integer::New(I, 0));
+            info.GetReturnValue().Set(iter);
+        }
+
+        static constexpr template_t iterator_object_template = iterator_obj_templ;
+    };
+
+    /// ===========================================================================
+    ///  Object templates.
+    /// ===========================================================================
+    /// Accessors for elements.
+    struct $element : object<element, &eval::element_tmpl> {
         /// Get attributes of an element.
         static void get_attributes(Local<String>, const PropertyCallbackInfo<Value>& info) {
             auto I = info.GetIsolate();
@@ -47,17 +114,26 @@ struct eval_impl {
 
             /// Create wrapper.
             Local<Object> obj = instantiate(I, context(I)->attributes_tmpl);
-            obj->SetInternalField(0, External::New(I, ptr(info)));
+            obj->SetInternalField(0, External::New(I, handle(info)));
             info.GetReturnValue().Set(obj);
         }
 
         /// Get children of an element.
+        static void get_children(Local<String>, const PropertyCallbackInfo<Value>& info) {
+            auto I = info.GetIsolate();
+            HandleScope hs{I};
+
+            /// Create wrapper.
+            Local<Object> obj = instantiate(I, context(I)->children_tmpl);
+            obj->SetInternalField(0, External::New(I, handle(info)));
+            info.GetReturnValue().Set(obj);
+        }
 
         /// Get ID of an element.
         static void get_id(Local<String>, const PropertyCallbackInfo<Value>& info) {
             auto I = info.GetIsolate();
             HandleScope hs{I};
-            info.GetReturnValue().Set(S(I, ptr(info)->id));
+            info.GetReturnValue().Set(S(I, handle(info)->id));
         }
 
         /// Set attributes of an element.
@@ -76,7 +152,7 @@ struct eval_impl {
             }
 
             /// Set attributes.
-            auto e = ptr(info);
+            auto e = handle(info);
             auto obj = new_attrs->ToObject(I->GetCurrentContext()).ToLocalChecked();
             auto ctx = I->GetCurrentContext();
             auto names = obj->GetOwnPropertyNames(ctx).ToLocalChecked();
@@ -98,168 +174,49 @@ struct eval_impl {
             auto I = info.GetIsolate();
             HandleScope hs{I};
             String::Utf8Value utf8{I, s->ToString(I->GetCurrentContext()).ToLocalChecked()};
-            ptr(info)->id = trim(std::string{*utf8, size_t(utf8.length())});
+            handle(info)->id = trim(std::string{*utf8, size_t(utf8.length())});
         }
     };
 
     /// Accessors for attributes.
-    class $attributes {
-        static auto ptr(const auto& info) -> element* {
-            return static_cast<element*>(
-                Local<External>::Cast(info.Holder()->GetInternalField(0))->Value()
-            );
-        }
-
-    public:
-        static void iterator(const FunctionCallbackInfo<Value>& info) {
-            auto I = info.GetIsolate();
-            HandleScope hs{I};
-
-            /// Create iterator.
-            auto e = ptr(info);
-            auto iter = instantiate(I, context(I)->attributes_it_tmpl);
-            iter->SetInternalField(0, External::New(I, e));
-            iter->SetInternalField(1, Integer::New(I, 0));
-            info.GetReturnValue().Set(iter);
-        }
-    };
-
-    /// Accessors for attributes iterators.
-    class $attributes_iterator {
-        static auto ptr(const auto& info) -> element* {
-            return static_cast<element*>(
-                Local<External>::Cast(info.Holder()->GetInternalField(0))->Value()
-            );
-        }
-
-        static auto index(const auto& info) -> int {
-            return int(Local<Integer>::Cast(info.Holder()->GetInternalField(1))->Value());
-        }
-
-    public:
-        static void next(const FunctionCallbackInfo<Value>& info) {
-            auto I = info.GetIsolate();
-            HandleScope hs{I};
-
-            /// Get element and index.
-            auto e = ptr(info);
-            auto i = index(info);
-
-            /// Create return value.
-            auto obj = info.Holder();
-            auto ctx = I->GetCurrentContext();
-            auto ret = Object::New(I);
-            if (i < int(e->attributes.size())) {
-                auto it = std::next(e->attributes.begin(), i);
-                obj->SetInternalField(1, Integer::New(I, i + 1));
-                ret->Set(ctx, S(I, "done"), False(I)).Check();
-
-                /// Create key-value pair.
+    struct $attributes
+        : object<element, &eval::attributes_tmpl>
+        , iterable<$attributes, &eval::attributes_it_tmpl> {
+        struct iterator {
+            static bool done(handle_type* e, int index) { return index >= int(e->attributes.size()); }
+            static auto value(Isolate* I, handle_type* e, int index) {
+                auto it = std::next(e->attributes.begin(), index);
                 auto pair = Array::New(I, 2);
+                auto ctx = I->GetCurrentContext();
                 pair->Set(ctx, 0, S(I, it->first)).Check();
                 pair->Set(ctx, 1, S(I, it->second)).Check();
-                ret->Set(ctx, S(I, "value"), pair).Check();
-            } else {
-                ret->Set(ctx, S(I, "done"), True(I)).Check();
+                return pair;
             }
-            info.GetReturnValue().Set(ret);
-        }
+        };
     };
 
-    /// Initialise V8.
-    eval_impl(parser& _p)
-        : p{_p} {
-        V8::InitializeICU();
-        V8::InitializeExternalStartupData(nullptr);
-        v8_platform = platform::NewDefaultPlatform();
-        V8::InitializePlatform(v8_platform.get());
-        V8::Initialize();
+    /// Accessors for children of an element.
+    struct $children
+        : object<element, &eval::children_tmpl>
+        , iterable<$children, &eval::children_it_tmpl> {
+        struct iterator {
+            static bool done(handle_type* e, int index) {
+                auto ch = std::get_if<element::vector>(&e->content);
+                return not ch or index >= int(ch->size());
+            }
 
-        /// Create isolate.
-        Isolate::CreateParams create_params;
-        create_params.array_buffer_allocator = alloc = ArrayBuffer::Allocator::NewDefaultAllocator();
-        auto I = isolate = Isolate::New(create_params);
-        I->SetData(0, this);
+            static auto value(Isolate* I, handle_type* e, int index) {
+                auto it = std::next(std::get<element::vector>(e->content).begin(), index);
+                auto r = context(I)->element_tmpl.Get(I)->NewInstance(I->GetCurrentContext()).ToLocalChecked();
+                r->SetInternalField(0, External::New(I, it->get()));
+                return r;
+            }
+        };
+    };
 
-        /// Template for global object.
-        HandleScope hs{I};
-        auto g_tm = ObjectTemplate::New(I);
-        globl_tmpl.Reset(I, g_tm);
-
-        /// Set globals.
-        export_global<$print>(g_tm, "print");
-        export_global<$$>(g_tm, "$");
-
-        /// Template for element wrapper objects.
-        auto elem_tm = ObjectTemplate::New(I);
-        element_tmpl.Reset(I, elem_tm);
-        elem_tm->SetInternalFieldCount(1);
-        elem_tm->SetAccessor(
-            S(I, "attributes"),
-            $element::get_attributes,
-            $element::set_attributes
-        );
-
-        elem_tm->SetAccessor(
-            S(I, "children"),
-            $element::get_children,
-            $element::set_children
-        );
-
-        elem_tm->SetAccessor(
-            S(I, "id"),
-            $element::get_id,
-            $element::set_id
-        );
-
-        /// Template for attribute wrapper objects.
-        auto attrs_tm = ObjectTemplate::New(I);
-        attributes_tmpl.Reset(I, attrs_tm);
-        attrs_tm->SetInternalFieldCount(1);
-        attrs_tm->Set(
-            Symbol::GetIterator(I),
-            FunctionTemplate::New(I, $attributes::iterator)
-        );
-
-        /// Template for attribute iterator objects.
-        auto attrs_it_tm = ObjectTemplate::New(I);
-        attributes_it_tmpl.Reset(I, attrs_it_tm);
-        attrs_it_tm->SetInternalFieldCount(2);
-        attrs_it_tm->Set(
-            S(I, "next"),
-            FunctionTemplate::New(I, $attributes_iterator::next)
-        );
-    }
-
-    ~eval_impl() {
-        isolate->Dispose();
-        delete alloc;
-    }
-
-    /// Get the parser from the isolate.
-    static auto context(Isolate* I) -> eval_impl* { return static_cast<eval_impl*>(I->GetData(0)); }
-
-    /// Create a new JS object from a template.
-    static auto instantiate(Isolate* I, Persistent<ObjectTemplate>& templ) -> Local<Object> {
-        return templ.Get(I)->NewInstance(I->GetCurrentContext()).ToLocalChecked();
-    }
-
-    /// Create a new string.
-    static Local<String> S(Isolate* I, auto&& str) {
-        if constexpr (requires { str.data(); }) {
-            auto s = String::NewFromUtf8(
-                I,
-                std::forward<decltype(str)>(str).data(),
-                NewStringType::kNormal,
-                int(std::forward<decltype(str)>(str).size())
-            );
-
-            return s.ToLocalChecked();
-        } else {
-            return String::NewFromUtf8(I, std::forward<decltype(str)>(str)).ToLocalChecked();
-        }
-    }
-
+    /// ===========================================================================
+    ///  Global functions.
+    /// ===========================================================================
     /// Query selector.
     static Local<Value> $$(const FunctionCallbackInfo<Value>& info) {
         auto I = info.GetIsolate();
@@ -294,6 +251,124 @@ struct eval_impl {
             }
         }
         return {};
+    }
+
+    /// ===========================================================================
+    ///  Initialisation.
+    /// ===========================================================================
+    eval_impl(parser& _p)
+        : p{_p} {
+        V8::InitializeICU();
+        V8::InitializeExternalStartupData(nullptr);
+        v8_platform = platform::NewDefaultPlatform();
+        V8::InitializePlatform(v8_platform.get());
+        V8::Initialize();
+
+        /// Create isolate.
+        Isolate::CreateParams create_params;
+        create_params.array_buffer_allocator = alloc = ArrayBuffer::Allocator::NewDefaultAllocator();
+        auto I = isolate = Isolate::New(create_params);
+        I->SetData(0, this);
+
+        /// Template for global object.
+        HandleScope hs{I};
+        auto g_tm = ObjectTemplate::New(I);
+        globl_tmpl.Reset(I, g_tm);
+
+        /// Set globals.
+        export_global<$print>(g_tm, "print");
+        export_global<$$>(g_tm, "$");
+
+        /// Template for element wrapper objects.
+        auto elem_tm = register_template<$element>();
+        register_template<$attributes>();
+        register_template<$children>();
+
+        elem_tm->SetAccessor(
+            S(I, "attributes"),
+            $element::get_attributes,
+            $element::set_attributes
+        );
+
+        elem_tm->SetAccessor(
+            S(I, "children"),
+            $element::get_children
+        );
+
+        elem_tm->SetAccessor(
+            S(I, "id"),
+            $element::get_id,
+            $element::set_id
+        );
+    }
+
+    /// ===========================================================================
+    ///  Various helpers.
+    /// ===========================================================================
+    ~eval_impl() {
+        isolate->Dispose();
+        delete alloc;
+    }
+
+    /// Get the parser from the isolate.
+    static auto context(Isolate* I) -> eval_impl* { return static_cast<eval_impl*>(I->GetData(0)); }
+
+    /// Create a new JS object from a template.
+    static auto instantiate(Isolate* I, Persistent<ObjectTemplate>& templ) -> Local<Object> {
+        return templ.Get(I)->NewInstance(I->GetCurrentContext()).ToLocalChecked();
+    }
+
+    /// Create an object template.
+    template <typename backing_type>
+    auto register_template() -> Local<ObjectTemplate> {
+        /// Sanity check.
+        if (not(this->*backing_type::object_template).IsEmpty()) {
+            fmt::print(stderr, "Internal error: template already registered\n");
+            fmt::print(stderr, "In: {}\n", __PRETTY_FUNCTION__);
+            std::abort();
+        }
+
+        /// Create template.
+        auto I = isolate;
+        auto tm = ObjectTemplate::New(I);
+        (this->*backing_type::object_template).Reset(I, tm);
+        tm->SetInternalFieldCount(backing_type::field_count);
+
+        /// Iterables.
+        if constexpr (requires { backing_type::create_iterator; }) {
+            static_assert(backing_type::field_count >= 1, "Iterable types must a backing native handle");
+            tm->Set(
+                Symbol::GetIterator(I),
+                FunctionTemplate::New(I, backing_type::create_iterator)
+            );
+
+            /// Iterator type.
+            auto iterator_tm = ObjectTemplate::New(I);
+            iterator_tm->SetInternalFieldCount(2);
+            (this->*backing_type::iterator_object_template).Reset(I, iterator_tm);
+            iterator_tm->Set(
+                S(I, "next"),
+                FunctionTemplate::New(I, iterator_base<typename backing_type::iterator, typename backing_type::handle_type>::next)
+            );
+        }
+
+        return tm;
+    }
+
+    /// Create a new string.
+    static Local<String> S(Isolate* I, auto&& str) {
+        if constexpr (requires { str.data(); }) {
+            auto s = String::NewFromUtf8(
+                I,
+                std::forward<decltype(str)>(str).data(),
+                NewStringType::kNormal,
+                int(std::forward<decltype(str)>(str).size())
+            );
+
+            return s.ToLocalChecked();
+        } else {
+            return String::NewFromUtf8(I, std::forward<decltype(str)>(str)).ToLocalChecked();
+        }
     }
 };
 
